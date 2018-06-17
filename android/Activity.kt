4941162255c13ca8536a8d6631d93db406f0e4eb
Blast Piruin Panichphol <piruin.p@gmail.com>
2018-06-17 14:51:39 +0700

    Update Activity.kt

--- android/Activity.kt
+++ android/Activity.kt
@@ -1,8 +1,15 @@
 import android.app.Activity
+import android.os.Build
+import android.os.Bundle
+import android.support.v4.app.ActivityOptionsCompat
+import android.view.View
+import android.support.v4.util.Pair as AndroidSupportPair
 
 fun Activity?.isActive() = this != null && !this.isFinishing() && !this.isDestroyed()
 
-fun Activity.sceneTransition(pair: Pair<View, String>): Bundle? {
+fun Activity.sceneTransition(vararg sharedElements: Pair<View, String>): Bundle? {
     return if (Build.VERSION.SDK_INT < 21) null else
-        ActivityOptionsCompat.makeSceneTransitionAnimation(this, pair.first, pair.second).toBundle()
+        ActivityOptionsCompat.makeSceneTransitionAnimation(this, *sharedElements.map { it.toAndroidSupportPair() }.toTypedArray()).toBundle()
 }
+
+private fun Pair<View, String>.toAndroidSupportPair() = AndroidSupportPair(first, second)
