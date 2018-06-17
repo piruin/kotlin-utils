c616f9b80da2a8a125dbd43db68d7e242f880a90
Blast Piruin Panichphol <piruin.p@gmail.com>
2018-06-17 14:21:26 +0700

    Update Activity.kt

--- android/Activity.kt
+++ android/Activity.kt
@@ -1,3 +1,8 @@
 import android.app.Activity
 
 fun Activity?.isActive() = this != null && !this.isFinishing() && !this.isDestroyed()
+
+fun Activity.sceneTransition(pair: Pair<View, String>): Bundle? {
+    return if (Build.VERSION.SDK_INT < 21) null else
+        ActivityOptionsCompat.makeSceneTransitionAnimation(this, pair.first, pair.second).toBundle()
+}
