e916f305b6ccdb594a4010085614aa80518c94d2
Blast Piruin Panichphol <piruin.p@gmail.com>
2018-07-05 15:10:57 +0700

    Add Drawable.toBitmap() and Context.drawble()
    toBitmap() by André at https://stackoverflow.com/a/10600736

--- android/Drawable.kt
+++ android/Drawable.kt
@@ -1,7 +1,40 @@
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.PorterDuff
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
 import android.support.annotation.ColorInt
+import android.support.annotation.DrawableRes
 
-fun Drawable.tint(@ColorInt color: Int, mode: PorterDuff.Mode = PorterDuff.Mode.SRC_IN) {
-  mutate().setColorFilter(color, mode)
+fun Drawable.tint(@ColorInt color: Int, mode: PorterDuff.Mode = PorterDuff.Mode.SRC_IN): Drawable {
+  return apply { mutate().setColorFilter(color, mode) }
+}
+
+fun Drawable.toBitmap(): Bitmap {
+  if (this is BitmapDrawable && bitmap != null) {
+    return bitmap
+  }
+
+  val bitmap: Bitmap = if (intrinsicWidth <= 0 || intrinsicHeight <= 0) {
+    Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
+  } else {
+    Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+  }
+
+  val canvas = Canvas(bitmap)
+  setBounds(0, 0, canvas.width, canvas.height)
+  draw(canvas)
+  return bitmap
+}
+
+fun Context.drawable(@DrawableRes drawableId: Int): Drawable {
+  return if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+    resources.getDrawable(drawableId, theme)
+  } else {
+    @Suppress("DEPRECATION")
+    resources.getDrawable(drawableId)
+  }
 }
