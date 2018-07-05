7a8bfee969bae968f2fc73004924d27c814c5fab
Blast Piruin Panichphol <piruin.p@gmail.com>
2018-07-05 15:21:15 +0700

    Update Color.kt

--- android/Color.kt
+++ android/Color.kt
@@ -1,4 +1,10 @@
+import android.content.Context
 import android.graphics.Color
+import android.support.annotation.ColorInt
+import android.support.annotation.ColorRes
+import android.support.v4.app.Fragment
+import android.support.v4.content.res.ResourcesCompat
+import android.view.View
 
 fun Int.toHue(): Float = toHSV()[0]
 
@@ -7,3 +13,7 @@ fun Int.toHSV(): FloatArray {
   Color.colorToHSV(this, hsv)
   return hsv
 }
+
+@ColorInt fun Context.color(@ColorRes colorRes: Int) = ResourcesCompat.getColor(resources, colorRes, theme)
+@ColorInt fun Fragment.color(@ColorRes colorRes: Int) = context!!.color(colorRes)
+@ColorInt fun View.color(@ColorRes colorRes: Int) = context.color(colorRes)
