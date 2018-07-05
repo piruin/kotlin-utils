f36c2056c26be4ab7d66a16507786f56aca61734
Blast Piruin Panichphol <piruin.p@gmail.com>
2018-07-05 15:19:16 +0700

    Update Resource.kt

--- android/Resource.kt
+++ android/Resource.kt
@@ -1,19 +1,35 @@
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.PorterDuff
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.support.annotation.ColorInt
-import android.support.annotation.ColorRes
 import android.support.annotation.DrawableRes
 import android.support.v4.app.Fragment
 import android.support.v4.content.res.ResourcesCompat
 import android.view.View
 
-@ColorInt
-fun Context.color(@ColorRes colorRes: Int) = ResourcesCompat.getColor(resources, colorRes, theme)
+fun Drawable.tint(@ColorInt color: Int, mode: PorterDuff.Mode = PorterDuff.Mode.SRC_IN): Drawable {
+  return apply { mutate().setColorFilter(color, mode) }
+}
 
-@ColorInt
-fun Fragment.color(@ColorRes colorRes: Int) = context!!.color(colorRes)
+fun Drawable.toBitmap(): Bitmap {
+  if (this is BitmapDrawable && bitmap != null) {
+    return bitmap
+  }
 
-@ColorInt
-fun View.color(@ColorRes colorRes: Int) = context.color(colorRes)
+  val bitmap: Bitmap = if (intrinsicWidth <= 0 || intrinsicHeight <= 0) {
+    Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)
+  } else {
+    Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, Bitmap.Config.ARGB_8888)
+  }
+
+  val canvas = Canvas(bitmap)
+  setBounds(0, 0, canvas.width, canvas.height)
+  draw(canvas)
+  return bitmap
+}
 
 fun Context.drawable(@DrawableRes drawableRes: Int) = ResourcesCompat.getDrawable(resources, drawableRes, theme)
 fun Fragment.drawable(@DrawableRes drawableRes: Int) = context!!.drawable(drawableRes)
