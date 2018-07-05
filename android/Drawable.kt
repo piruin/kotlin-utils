2dfeb504171f6a00a2884fadc61cd11053624e5e
Blast Piruin Panichphol <piruin.p@gmail.com>
2018-07-05 15:19:35 +0700

    Update Drawable.kt

--- android/Drawable.kt
+++ android/Drawable.kt
@@ -1,16 +1,13 @@
-
 import android.content.Context
-import android.content.res.Resources.Theme
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.PorterDuff
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
-import android.os.Build.VERSION
-import android.os.Build.VERSION_CODES
 import android.support.annotation.ColorInt
 import android.support.annotation.DrawableRes
 import android.support.v4.app.Fragment
+import android.support.v4.content.res.ResourcesCompat
 import android.view.View
 
 fun Drawable.tint(@ColorInt color: Int, mode: PorterDuff.Mode = PorterDuff.Mode.SRC_IN): Drawable {
@@ -34,14 +31,7 @@ fun Drawable.toBitmap(): Bitmap {
   return bitmap
 }
 
-fun Context.drawable(@DrawableRes drawableId: Int, theme: Theme? = null): Drawable {
-  return if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-    resources.getDrawable(drawableId, theme)
-  } else {
-    @Suppress("DEPRECATION")
-    resources.getDrawable(drawableId)
-  }
-}
 
-fun Fragment.drawable(@DrawableRes drawableRes: Int, theme: Theme? = null) = context!!.drawable(drawableRes, theme)
-fun View.drawable(@DrawableRes drawableRes: Int, theme: Theme? = null) = context.drawable(drawableRes, theme)
+fun Context.drawable(@DrawableRes drawableRes: Int) = ResourcesCompat.getDrawable(resources, drawableRes, theme)
+fun Fragment.drawable(@DrawableRes drawableRes: Int) = context!!.drawable(drawableRes)
+fun View.drawable(@DrawableRes drawableRes: Int) = context.drawable(drawableRes)
