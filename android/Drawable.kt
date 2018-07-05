6ef80a1dccbb0176ce59204337077257dfbbb253
Blast Piruin Panichphol <piruin.p@gmail.com>
2018-07-05 15:17:43 +0700

    Update Drawable.kt

--- android/Drawable.kt
+++ android/Drawable.kt
@@ -1,4 +1,6 @@
+
 import android.content.Context
+import android.content.res.Resources.Theme
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.PorterDuff
@@ -8,6 +10,8 @@ import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
 import android.support.annotation.ColorInt
 import android.support.annotation.DrawableRes
+import android.support.v4.app.Fragment
+import android.view.View
 
 fun Drawable.tint(@ColorInt color: Int, mode: PorterDuff.Mode = PorterDuff.Mode.SRC_IN): Drawable {
   return apply { mutate().setColorFilter(color, mode) }
@@ -30,7 +34,7 @@ fun Drawable.toBitmap(): Bitmap {
   return bitmap
 }
 
-fun Context.drawable(@DrawableRes drawableId: Int): Drawable {
+fun Context.drawable(@DrawableRes drawableId: Int, theme: Theme? = null): Drawable {
   return if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
     resources.getDrawable(drawableId, theme)
   } else {
@@ -38,3 +42,6 @@ fun Context.drawable(@DrawableRes drawableId: Int): Drawable {
     resources.getDrawable(drawableId)
   }
 }
+
+fun Fragment.drawable(@DrawableRes drawableRes: Int, theme: Theme? = null) = context!!.drawable(drawableRes, theme)
+fun View.drawable(@DrawableRes drawableRes: Int, theme: Theme? = null) = context.drawable(drawableRes, theme)
