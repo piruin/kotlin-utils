a4944bc5aa435ab569889df164e419d0984a9a78
Blast Piruin Panichphol <piruin.p@gmail.com>
2018-07-05 15:02:54 +0700

    Update Assert.kt

--- android/Assert.kt
+++ android/Assert.kt
@@ -1,5 +1,6 @@
-import android.app.Activity
 import android.app.Fragment
+import android.content.Context
+import android.support.annotation.StringRes
 import android.view.View
 
 fun Context.require(value: Boolean, @StringRes resId: Int) = require(value) { getString(resId) }
